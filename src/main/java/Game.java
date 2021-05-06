d54ccacc595dd49623c5d65fc641e1d1fdf58985
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-05-06 19:38:55 +0200

    Added basic main class

---
+++ src/main/java/Game.java
@@ -0,0 +1,14 @@
+/**
+ * The main class that starts the game/
+ */
+public class Game {
+    
+    /**
+     * The main method. Starts the game
+     * @param args the arguments passed to the main method
+     */
+    public static void main(String[] args){
+    
+        
+    }
+}
