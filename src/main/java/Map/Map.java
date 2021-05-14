057abacf084c8f6e56918ed7569aba5830afcaff
BojanUSI <lazarb@usi.ch>
2021-05-14 20:21:24 +0200

    Map Layout

--- src/main/java/Map/Map.java
+++ src/main/java/Map/Map.java
@@ -88,86 +88,218 @@ public class Map extends JPanel {
         to.setEdge(edge, toDirection);
         edges.add(edge);
     }
-    
-    
+
     private void createMap() {
         edges = new ArrayList<Edge>();
         nodes = new ArrayList<Node>();
-        
-        Node n11 = new Node(Scaler.scale(100),Scaler.scale(100));
-        Node n12 = new Node(Scaler.scale(400),Scaler.scale(100));
-        addEdge(n11, n12, EDirection.RIGHT, EDirection.LEFT);
-    
-        Node n13 = new Node(Scaler.scale(600),Scaler.scale(100));
-        addEdge(n12,  n13, EDirection.RIGHT,EDirection.LEFT);
-    
-        Node n14 = new Node(Scaler.scale(900),Scaler.scale(100));
-        addEdge(n13,  n14, EDirection.RIGHT,EDirection.LEFT);
-    
-    
-    
-    
-    
-        Node n21 = new Node(Scaler.scale(100),Scaler.scale(300));
-        Node n22 = new Node(Scaler.scale(400),Scaler.scale(300));
-        addEdge(n21, n22, EDirection.RIGHT,EDirection.LEFT);
-    
-        Node n23 = new Node(Scaler.scale(600),Scaler.scale(300));
-    
-        Node n24 = new Node(Scaler.scale(900),Scaler.scale(300));
-        addEdge(n23, n24, EDirection.RIGHT,EDirection.LEFT);
-    
-        addEdge(n11,n21,EDirection.DOWN,EDirection.UP);
-        addEdge(n12,n22,EDirection.DOWN,EDirection.UP);
-        addEdge(n13,n23,EDirection.DOWN,EDirection.UP);
-        addEdge(n14,n24,EDirection.DOWN,EDirection.UP);
-    
-    
-    
-    
-        Node n32 = new Node(Scaler.scale(400),Scaler.scale(500));
-        Node n33 = new Node(Scaler.scale(600),Scaler.scale(500));
-        addEdge(n32, n33, EDirection.RIGHT,EDirection.LEFT);
-    
-        addEdge(n22,n32,EDirection.DOWN,EDirection.UP);
-        addEdge(n23,n33,EDirection.DOWN,EDirection.UP);
-    
-    
-    
-    
-    
-        Node n41 = new Node(Scaler.scale(100),Scaler.scale(700));
-        Node n42 = new Node(Scaler.scale(400),Scaler.scale(700));
-        addEdge(n41, n42, EDirection.RIGHT, EDirection.LEFT);
-    
-        Node n43 = new Node(Scaler.scale(600),Scaler.scale(700));
-    
-        Node n44 = new Node(Scaler.scale(900),Scaler.scale(700));
-        addEdge(n43,  n44, EDirection.RIGHT,EDirection.LEFT);
-    
-        addEdge(n41,n21,EDirection.UP,EDirection.DOWN);
-        addEdge(n42,n32,EDirection.UP,EDirection.DOWN);
-        addEdge(n43,n33,EDirection.UP,EDirection.DOWN);
-        addEdge(n44,n24,EDirection.UP,EDirection.DOWN);
-    
-    
-    
-    
-    
-        Node n51 = new Node(Scaler.scale(100),Scaler.scale(900));
-        Node n52 = new Node(Scaler.scale(400),Scaler.scale(900));
-        addEdge(n51, n52, EDirection.RIGHT,EDirection.LEFT);
-    
-        Node n53 = new Node(Scaler.scale(600),Scaler.scale(900));
-        addEdge(n52, n53, EDirection.RIGHT,EDirection.LEFT);
-    
-        Node n54 = new Node(Scaler.scale(900),Scaler.scale(900));
-        addEdge(n53, n54, EDirection.RIGHT,EDirection.LEFT);
-    
-        addEdge(n41,n51,EDirection.DOWN,EDirection.UP);
-        addEdge(n42,n52,EDirection.DOWN,EDirection.UP);
-        addEdge(n43,n53,EDirection.DOWN,EDirection.UP);
-        addEdge(n44,n54,EDirection.DOWN,EDirection.UP);
+
+        Node r1c1 = new Node(Scaler.scale(50), Scaler.scale(50));
+        Node r1c2 = new Node(Scaler.scale(250), Scaler.scale(50));
+        addEdge(r1c1, r1c2, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r1c4 = new Node(Scaler.scale(470), Scaler.scale(50));
+        addEdge(r1c2, r1c4, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r2c1 = new Node(Scaler.scale(50), Scaler.scale(200));
+        addEdge(r1c1, r2c1, EDirection.DOWN, EDirection.UP);
+
+        Node r2c2 = new Node(Scaler.scale(250), Scaler.scale(200));
+        addEdge(r2c1, r2c2, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r1c2, r2c2, EDirection.DOWN, EDirection.UP);
+
+        Node r2c3 = new Node(Scaler.scale(360), Scaler.scale(200));
+        addEdge(r2c2, r2c3, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r2c4 = new Node(Scaler.scale(470), Scaler.scale(200));
+        addEdge(r2c3, r2c4, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r1c4, r2c4, EDirection.DOWN, EDirection.UP);
+
+        Node r3c1 = new Node(Scaler.scale(50), Scaler.scale(300));
+        addEdge(r2c1, r3c1, EDirection.DOWN, EDirection.UP);
+
+        Node r3c2 = new Node(Scaler.scale(250), Scaler.scale(300));
+        addEdge(r3c1, r3c2, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r2c2, r3c2, EDirection.DOWN, EDirection.UP);
+
+        Node r3c3 = new Node(Scaler.scale(360), Scaler.scale(300));
+        addEdge(r2c3,r3c3, EDirection.DOWN, EDirection.UP);
+
+        Node r3c4 = new Node(Scaler.scale(470), Scaler.scale(300));
+        addEdge(r3c3, r3c4, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r1c5 = new Node(Scaler.scale(530), Scaler.scale(50));
+        Node r1c7 = new Node(Scaler.scale(750), Scaler.scale(50));
+        addEdge(r1c5, r1c7, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r1c8 = new Node(Scaler.scale(950), Scaler.scale(50));
+        addEdge(r1c7, r1c8, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r2c5 = new Node(Scaler.scale(530), Scaler.scale(200));
+        addEdge(r2c4, r2c5, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r1c5, r2c5, EDirection.DOWN, EDirection.UP);
+
+        Node r2c6 = new Node(Scaler.scale(640), Scaler.scale(200));
+        addEdge(r2c5, r2c6, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r2c7 = new Node(Scaler.scale(750), Scaler.scale(200));
+        addEdge(r2c6, r2c7, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r1c7, r2c7, EDirection.DOWN, EDirection.UP);
+
+        Node r2c8 = new Node(Scaler.scale(950), Scaler.scale(200));
+        addEdge(r2c7, r2c8, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r1c8, r2c8, EDirection.DOWN, EDirection.UP);
+
+        Node r3c5 = new Node(Scaler.scale(530), Scaler.scale(300));
+        Node r3c6 = new Node(Scaler.scale(640), Scaler.scale(300));
+        addEdge(r3c5, r3c6, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r2c6, r3c6, EDirection.DOWN, EDirection.UP);
+
+        Node r3c7 = new Node(Scaler.scale(750), Scaler.scale(300));
+        addEdge(r2c7, r3c7, EDirection.DOWN, EDirection.UP);
+
+        Node r3c8 = new Node(Scaler.scale(950), Scaler.scale(300));
+        addEdge(r3c7, r3c8, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r2c8, r3c8, EDirection.DOWN, EDirection.UP);
+
+        Node r4c3 = new Node(Scaler.scale(360), Scaler.scale(400));
+        Node r4c4 = new Node(Scaler.scale(470), Scaler.scale(400));
+        addEdge(r4c3, r4c4, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r3c4, r4c4, EDirection.DOWN, EDirection.UP);
+
+        Node r4c5 = new Node(Scaler.scale(530), Scaler.scale(400));
+        addEdge(r4c4, r4c5, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r3c5, r4c5, EDirection.DOWN, EDirection.UP);
+
+        Node r4c6 = new Node(Scaler.scale(640), Scaler.scale(400));
+        addEdge(r4c5, r4c6, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r5c2 = new Node(Scaler.scale(250), Scaler.scale(500));
+        addEdge(r3c2, r5c2, EDirection.DOWN, EDirection.UP);
+
+        Node r5c3 = new Node(Scaler.scale(360), Scaler.scale(500));
+        addEdge(r5c2, r5c3, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r4c3, r5c3, EDirection.DOWN, EDirection.UP);
+
+        Node r5c6 = new Node(Scaler.scale(640), Scaler.scale(500));
+        addEdge(r4c6, r5c6, EDirection.DOWN, EDirection.UP);
+
+        Node r5c7 = new Node(Scaler.scale(750), Scaler.scale(500));
+        addEdge(r5c6, r5c7, EDirection.RIGHT, EDirection.LEFT);
+
+        Node empty1 = new Node(Scaler.scale(0), Scaler.scale(500));
+        addEdge(empty1, r5c2, EDirection.RIGHT, EDirection.LEFT);
+
+        Node empty2 = new Node(Scaler.scale(1000), Scaler.scale(500));
+        addEdge(r5c7, empty2, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r3c7, r5c7, EDirection.DOWN, EDirection.UP);
+
+        Node r6c3 = new Node(Scaler.scale(360),Scaler.scale(600));
+        addEdge(r5c3, r6c3, EDirection.DOWN, EDirection.UP);
+
+        Node r6c6 = new Node(Scaler.scale(640), Scaler.scale(600));
+        addEdge(r6c3, r6c6, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r5c6, r6c6, EDirection.DOWN, EDirection.UP);
+
+        Node r7c1 = new Node(Scaler.scale(50),Scaler.scale(700));
+        Node r7c2 = new Node(Scaler.scale(250), Scaler.scale(700));
+        addEdge(r7c1, r7c2, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r5c2, r7c2, EDirection.DOWN, EDirection.UP);
+
+        Node r7c3 = new Node(Scaler.scale(360), Scaler.scale(700));
+        addEdge(r7c2, r7c3, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r6c3, r7c3, EDirection.DOWN, EDirection.UP);
+
+        Node r7c4 = new Node(Scaler.scale(470), Scaler.scale(700));
+        addEdge(r7c3, r7c4, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r7c5 = new Node(Scaler.scale(530), Scaler.scale(700));
+        Node r7c6 = new Node(Scaler.scale(640), Scaler.scale(700));
+        addEdge(r7c5, r7c6, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r6c6, r7c6, EDirection.DOWN, EDirection.UP);
+
+        Node r7c7 = new Node(Scaler.scale(750), Scaler.scale(700));
+        addEdge(r7c6, r7c7, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r5c7, r7c7, EDirection.DOWN, EDirection.UP);
+
+        Node r7c8 = new Node(Scaler.scale(950), Scaler.scale(700));
+        addEdge(r7c7, r7c8, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r8c1 = new Node(Scaler.scale(50), Scaler.scale(780));
+        Node r8c2_half = new Node(Scaler.scale(150), Scaler.scale(780));
+        addEdge(r8c1, r8c2_half, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r7c1, r8c1, EDirection.DOWN, EDirection.UP);
+
+        Node r8c2 = new Node(Scaler.scale(250), Scaler.scale(780));
+        addEdge(r7c2, r8c2, EDirection.DOWN, EDirection.UP);
+
+        Node r8c3 = new Node(Scaler.scale(360), Scaler.scale(780));
+        addEdge(r8c2, r8c3, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r8c4 = new Node(Scaler.scale(470), Scaler.scale(780));
+        addEdge(r8c3, r8c4, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r7c4, r8c4, EDirection.DOWN, EDirection.UP);
+
+        Node r8c5 = new Node(Scaler.scale(530), Scaler.scale(780));
+        addEdge(r8c4, r8c5, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r7c5, r8c5, EDirection.DOWN, EDirection.UP);
+
+        Node r8c6 = new Node(Scaler.scale(640), Scaler.scale(780));
+        addEdge(r8c5, r8c6, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r8c7 = new Node(Scaler.scale(750), Scaler.scale(780));
+        addEdge(r8c6, r8c7, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r7c7, r8c7, EDirection.DOWN, EDirection.UP);
+
+        Node r8c7_half = new Node(Scaler.scale(850), Scaler.scale(780));
+        Node r8c8 = new Node(Scaler.scale(950), Scaler.scale(780));
+        addEdge(r8c7_half, r8c8, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r7c8, r8c8, EDirection.DOWN, EDirection.UP);
+
+        Node r9c1 = new Node(Scaler.scale(50), Scaler.scale(880));
+        Node r9c2_half = new Node(Scaler.scale(150), Scaler.scale(880));
+        addEdge(r9c1, r9c2_half, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r8c2_half, r9c2_half, EDirection.DOWN, EDirection.UP);
+
+        Node r9c2 = new Node(Scaler.scale(250), Scaler.scale(880));
+        addEdge(r9c2_half, r9c2, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r8c2, r9c2, EDirection.DOWN, EDirection.UP);
+
+        Node r9c3 = new Node(Scaler.scale(360), Scaler.scale(880));
+        addEdge(r8c3, r9c3, EDirection.DOWN, EDirection.UP);
+
+        Node r9c4 = new Node(Scaler.scale(470), Scaler.scale(880));
+        addEdge(r9c3, r9c4, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r9c5 = new Node(Scaler.scale(530), Scaler.scale(880));
+        Node r9c6 = new Node(Scaler.scale(640), Scaler.scale(880));
+        addEdge(r9c5, r9c6, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r8c6, r9c6, EDirection.DOWN, EDirection.UP);
+
+        Node r9c7 = new Node(Scaler.scale(750), Scaler.scale(880));
+        addEdge(r8c7, r9c7, EDirection.DOWN, EDirection.UP);
+
+        Node r9c7_half = new Node(Scaler.scale(850), Scaler.scale(880));
+        addEdge(r9c7, r9c7_half, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r8c7_half, r9c7_half, EDirection.DOWN, EDirection.UP);
+
+        Node r9c8 = new Node(Scaler.scale(950), Scaler.scale(880));
+        addEdge(r9c7_half, r9c8, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r10c1 = new Node(Scaler.scale(50), Scaler.scale(950));
+        addEdge(r9c1, r10c1, EDirection.DOWN, EDirection.UP);
+
+        Node r10c4 = new Node(Scaler.scale(470), Scaler.scale(950));
+        addEdge(r9c4, r10c4, EDirection.DOWN, EDirection.UP);
+        addEdge(r10c1, r10c4, EDirection.RIGHT, EDirection.LEFT);
+
+        Node r10c5 = new Node(Scaler.scale(530), Scaler.scale(950));
+        addEdge(r10c4, r10c5, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r9c5, r10c5, EDirection.DOWN, EDirection.UP);
+
+        Node r10c8 = new Node(Scaler.scale(950), Scaler.scale(950));
+        addEdge(r10c5, r10c8, EDirection.RIGHT, EDirection.LEFT);
+        addEdge(r9c8, r10c8, EDirection.DOWN, EDirection.UP);
+
     }
     
     //////////////////
