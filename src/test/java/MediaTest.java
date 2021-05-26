a3acc0617c469ad2ec521c23a8a27518e471687d
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-05-26 12:48:37 +0200

    Added tests for SFX media

--- src/test/java/MediaTest.java
+++ src/test/java/MediaTest.java
@@ -7,15 +7,28 @@ import Media.*;
 
 public class MediaTest {
     /**
-     * Checks that every possible media has been imported
+     * Checks that every possible image has been imported
      *  and available to use.
      */
     @Test
-    public void allMediaMapped() {
+    public void allImgMapped() {
         EnumMap m = Media.getImgMap();
         
         for(EImage image : EImage.values()) {
             assertTrue(m.containsKey(image));
+        }
+    }
+    
+    /**
+     * Checks that every possible SFX has been imported
+     *  and available to use.
+     */
+    @Test
+    public void allSfxMapped() {
+        EnumMap m = Media.getSfxMap();
+        
+        for(EAudio sfx : EAudio.values()) {
+            assertTrue(m.containsKey(sfx));
         }
     }
     
