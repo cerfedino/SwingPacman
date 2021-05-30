c6fa71611f58f9fbe6d05801036cca4624146d9c
BojanUSI <lazarb@usi.ch>
2021-05-30 19:07:35 +0200

    Collisions

--- src/main/java/Entities/EntityManager.java
+++ src/main/java/Entities/EntityManager.java
@@ -7,34 +7,36 @@ public class EntityManager {
     public static void checkCollisions(Entity e) {
         if (e instanceof Pacman) {
             for (Ghost g : Game.gamestate().getGhosts()) {
-                if ((g.getSpriteX() == e.getSpriteX()) || (g.getSpriteY() == e.getSpriteY())) {
-                    checkCollision(e, g);
-                    checkCollision(g, e);
+                if (areColliding(e, g)) {
+                    g.onCollision(e);
+                    e.onCollision(g);
                 }
             }
-            // check for food collision as well
+            for (Food f : e.getCurrEdge().getFood()) {
+                if(areColliding(e,f)){
+                    e.onCollision(f);
+                    f.onCollision(e);
+                }
+            }
         }
         if (e instanceof Ghost) {
             Pacman p = Game.gamestate().getPacman();
             if (e.getCurrEdge() == p.getCurrEdge()) {
-                checkCollision(p, e);
-                checkCollision(e, p);
+                p.onCollision(e);
+                e.onCollision(p);
             }
         }
     }
 
-    public static void checkCollision(Entity a, Entity b) {
+    public static boolean areColliding(Entity a, Entity b) {
         if (a.isColliding() && b.isColliding()) {
             if (a.getSpriteX() > b.getSpriteX() && ((a.getSpriteX() - a.getWidth()/2) - (b.getSpriteX()+b.getWidth()/2)) < 1) {
-                System.out.println("Collision");
-                a.onCollision(b);
-                b.onCollision(a);
+                return true;
             }
             if (a.getSpriteY() > b.getSpriteY() && ((a.getSpriteY() - a.getHeight()/2) - (b.getSpriteY()+b.getHeight()/2)) < 1) {
-                System.out.println("Collision");
-                a.onCollision(b);
-                b.onCollision(a);
+                return true;
             }
         }
+        return false;
     }
 }
