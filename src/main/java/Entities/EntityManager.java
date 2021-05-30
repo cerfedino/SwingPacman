6ba83a32de08a630e048a4d73319bdac0a6526a2
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-05-30 18:36:18 +0200

    Updated collision method

--- src/main/java/Entities/EntityManager.java
+++ src/main/java/Entities/EntityManager.java
@@ -1,11 +1,6 @@
 package Entities;
 
-
 import Game.Game;
-
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
 
 public class EntityManager {
 
@@ -30,19 +25,15 @@ public class EntityManager {
 
     public static void checkCollision(Entity a, Entity b) {
         if (a.isColliding() && b.isColliding()) {
-            if (a.getSpriteX() > b.getSpriteX()) {
-                if (((a.getSpriteX() - a.getWidth()/2) - (b.getSpriteX()+b.getWidth()/2)) < 1) {
-                    System.out.println("Collision");
-                    a.onCollision(b);
-                    b.onCollision(a);
-                }
+            if (a.getSpriteX() > b.getSpriteX() && ((a.getSpriteX() - a.getWidth()/2) - (b.getSpriteX()+b.getWidth()/2)) < 1) {
+                System.out.println("Collision");
+                a.onCollision(b);
+                b.onCollision(a);
             }
-            if (a.getSpriteY() > b.getSpriteY()) {
-                if (((a.getSpriteY() - a.getHeight()/2) - (b.getSpriteY()+b.getHeight()/2)) < 1) {
-                    System.out.println("Collision");
-                    a.onCollision(b);
-                    b.onCollision(a);
-                }
+            if (a.getSpriteY() > b.getSpriteY() && ((a.getSpriteY() - a.getHeight()/2) - (b.getSpriteY()+b.getHeight()/2)) < 1) {
+                System.out.println("Collision");
+                a.onCollision(b);
+                b.onCollision(a);
             }
         }
     }
