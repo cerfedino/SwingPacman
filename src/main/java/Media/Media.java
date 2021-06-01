a46e79ad5b9463a9eac24e23978fd4daa49b25ea
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-06-01 19:02:23 +0200

    Added base scale to media in order to quickly resize all the Media by a baseScale, allowing to have way higher resolution Sprites.

--- src/main/java/Media/Media.java
+++ src/main/java/Media/Media.java
@@ -61,67 +61,69 @@ public class Media {
      * Imports the media files in the class.
      */
     static private void importMedia() throws IOException  {
-    
+        
         EnumMap<EImage, BufferedImage> newImage = new EnumMap<>(EImage.class);
         EnumMap<EAudio, File> newSfx = new EnumMap<>(EAudio.class);
         EnumMap<EFont, Font> newFont = new EnumMap<>(EFont.class);
-    
+        
+        
+        double baseScale = 0.25;
         newImage.put(EImage.placeholder, ImageIO.read(new File("./src/main/resources/img/placeholder.png")));
     
         //////////////////////////////
     
-        newImage.put(EImage.pacman_left_1, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_1.png")));
-        newImage.put(EImage.pacman_right_1, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_1.png")));
-        newImage.put(EImage.pacman_up_1, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_1.png")));
-        newImage.put(EImage.pacman_down_1, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_1.png")));
+        newImage.put(EImage.pacman_left_1, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_1.png")), baseScale));
+        newImage.put(EImage.pacman_right_1, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_1.png")), baseScale));
+        newImage.put(EImage.pacman_up_1, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_1.png")), baseScale));
+        newImage.put(EImage.pacman_down_1, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_1.png")), baseScale));
     
-        newImage.put(EImage.pacman_left_2, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_2.png")));
-        newImage.put(EImage.pacman_right_2, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_2.png")));
-        newImage.put(EImage.pacman_up_2, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_2.png")));
-        newImage.put(EImage.pacman_down_2, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_2.png")));
+        newImage.put(EImage.pacman_left_2, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_2.png")), baseScale));
+        newImage.put(EImage.pacman_right_2, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_2.png")), baseScale));
+        newImage.put(EImage.pacman_up_2, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_2.png")), baseScale));
+        newImage.put(EImage.pacman_down_2, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_2.png")), baseScale));
     
-        newImage.put(EImage.pacman_left_3, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_3.png")));
-        newImage.put(EImage.pacman_right_3, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_3.png")));
-        newImage.put(EImage.pacman_up_3, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_3.png")));
-        newImage.put(EImage.pacman_down_3, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_3.png")));
+        newImage.put(EImage.pacman_left_3, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_3.png")), baseScale));
+        newImage.put(EImage.pacman_right_3, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_3.png")), baseScale));
+        newImage.put(EImage.pacman_up_3, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_3.png")), baseScale));
+        newImage.put(EImage.pacman_down_3, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_3.png")), baseScale));
     
-        newImage.put(EImage.pacman_left_4, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_4.png")));
-        newImage.put(EImage.pacman_right_4, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_4.png")));
-        newImage.put(EImage.pacman_up_4, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_4.png")));
-        newImage.put(EImage.pacman_down_4, ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_4.png")));
+        newImage.put(EImage.pacman_left_4, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_left_4.png")), baseScale));
+        newImage.put(EImage.pacman_right_4, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_right_4.png")), baseScale));
+        newImage.put(EImage.pacman_up_4, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_up_4.png")), baseScale));
+        newImage.put(EImage.pacman_down_4, scaleImg(ImageIO.read(new File("./src/main/resources/img/pacman/pacman_down_4.png")), baseScale));
         
         /////////////////////////////
     
-        newImage.put(EImage.ghost1_left, ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_left.png")));
-        newImage.put(EImage.ghost1_right, ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_right.png")));
-        newImage.put(EImage.ghost1_up, ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_up.png")));
-        newImage.put(EImage.ghost1_down, ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_down.png")));
+        newImage.put(EImage.ghost1_left, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_left.png")), baseScale));
+        newImage.put(EImage.ghost1_right, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_right.png")), baseScale));
+        newImage.put(EImage.ghost1_up, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_up.png")), baseScale));
+        newImage.put(EImage.ghost1_down, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost1_down.png")), baseScale));
         
     
-        newImage.put(EImage.ghost2_left, ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_left.png")));
-        newImage.put(EImage.ghost2_right, ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_right.png")));
-        newImage.put(EImage.ghost2_up, ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_up.png")));
-        newImage.put(EImage.ghost2_down, ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_down.png")));
+        newImage.put(EImage.ghost2_left, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_left.png")), baseScale));
+        newImage.put(EImage.ghost2_right, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_right.png")), baseScale));
+        newImage.put(EImage.ghost2_up, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_up.png")), baseScale));
+        newImage.put(EImage.ghost2_down, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost2_down.png")), baseScale));
         
         
-        newImage.put(EImage.ghost3_left, ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_left.png")));
-        newImage.put(EImage.ghost3_right, ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_right.png")));
-        newImage.put(EImage.ghost3_up, ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_up.png")));
-        newImage.put(EImage.ghost3_down, ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_down.png")));
+        newImage.put(EImage.ghost3_left, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_left.png")), baseScale));
+        newImage.put(EImage.ghost3_right, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_right.png")), baseScale));
+        newImage.put(EImage.ghost3_up, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_up.png")), baseScale));
+        newImage.put(EImage.ghost3_down, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost3_down.png")), baseScale));
         
         
-        newImage.put(EImage.ghost4_left, ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_left.png")));
-        newImage.put(EImage.ghost4_right, ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_right.png")));
-        newImage.put(EImage.ghost4_up, ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_up.png")));
-        newImage.put(EImage.ghost4_down, ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_down.png")));
+        newImage.put(EImage.ghost4_left, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_left.png")), baseScale));
+        newImage.put(EImage.ghost4_right, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_right.png")), baseScale));
+        newImage.put(EImage.ghost4_up, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_up.png")), baseScale));
+        newImage.put(EImage.ghost4_down, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost4_down.png")), baseScale));
         
     
-        newImage.put(EImage.ghost_vuln, ImageIO.read(new File("./src/main/resources/img/ghost/ghost_vuln.png")));
+        newImage.put(EImage.ghost_vuln, scaleImg(ImageIO.read(new File("./src/main/resources/img/ghost/ghost_vuln.png")), baseScale));
 
-        newImage.put(EImage.large_food, ImageIO.read(new File("./src/main/resources/img/large_food.png")));
-        newImage.put(EImage.small_food, ImageIO.read(new File("./src/main/resources/img/small_food.png")));
+        newImage.put(EImage.large_food, scaleImg(ImageIO.read(new File("./src/main/resources/img/large_food.png")), baseScale));
+        newImage.put(EImage.small_food, scaleImg(ImageIO.read(new File("./src/main/resources/img/small_food.png")), baseScale));
 
-        newImage.put(EImage.live, ImageIO.read(new File("./src/main/resources/img/live.png")));
+        newImage.put(EImage.live, scaleImg(ImageIO.read(new File("./src/main/resources/img/live.png")), baseScale));
         
         ////////////////////
         // Audio
